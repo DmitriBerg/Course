5c4ff6d501711bb5ecc5c4dec2b6e4009bf981da
Dmitri Berg <info@doberg.se>
2020-11-07 12:21:04 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const category = 'toys';
+
+console.log(`https://fwfre.com/${category}/5`);
+
+const user ='Ivan';
+
+alert(`Privet, ${user}`);
+
+//alert ('Hello');
+
+//const result = confirm('Are you here?');
+// console.log(result );
+
+//const answer = prompt('Are you over 18?', '19');
+//console.log(answer + 10);
+
+//const answers = [];
+
+// answers[0] = prompt('name?', '');
+// answers[1] = prompt('family?', '');
+// answers[2] = prompt('age?', '');
+
+//document.write(typeof(answers));
+
+//console.log(typeof(answers));
