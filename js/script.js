e41ff15a880d3939f5eec800194a80d01296d899
Dmitri Berg <info@doberg.se>
2020-11-07 15:05:55 +0100

    Try

--- js/script.js
+++ js/script.js
@@ -1,27 +1,11 @@
 "use strict";
 
-const category = 'toys';
+const user ='Hui';
 
-console.log(`https://fwfre.com/${category}/5`);
+// alert(`Privet, ${user}`);
 
-const user ='Ivan';
+// let incr = 10,
+//     decr = 10;
 
-alert(`Privet, ${user}`);
-
-//alert ('Hello');
-
-//const result = confirm('Are you here?');
-// console.log(result );
-
-//const answer = prompt('Are you over 18?', '19');
-//console.log(answer + 10);
-
-//const answers = [];
-
-// answers[0] = prompt('name?', '');
-// answers[1] = prompt('family?', '');
-// answers[2] = prompt('age?', '');
-
-//document.write(typeof(answers));
-
-//console.log(typeof(answers));
+//     incr++;
+//     decr--;
